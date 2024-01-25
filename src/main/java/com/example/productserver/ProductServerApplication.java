159bcbf72eedafc965a6122afc3318bc9164afaa
zxf123 <xiangfuz@cn.ibm.com>
2024-01-25 09:12:39 +0800

    code update

--- src/main/java/com/example/productserver/ProductServerApplication.java
+++ src/main/java/com/example/productserver/ProductServerApplication.java
@@ -3,11 +3,9 @@ package com.example.productserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableSwagger2
 public class ProductServerApplication {
 
     public static void main(String[] args) {
